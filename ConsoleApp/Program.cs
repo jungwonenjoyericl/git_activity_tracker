def01e56d77b9977b1b5c1571e7270e75ecd6330
KameraSaki <adjosdaniel108@gmail.com>
2026-02-06 22:33:11 +0100

    implement get methods
    figuring out C# aswell as githubs rest api
    next: move interaction to another module.
    
    maybe start with tracking activities aswell.

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -1,2 +1,54 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Net.Http;
+using System.Text.Json;
+using System.Net.Http.Headers;
+using DotNetEnv;
+
+Env.Load("../.env");
+
+string url = Environment.GetEnvironmentVariable("USER_URL");
+string token = Environment.GetEnvironmentVariable("GITHUB_TOKEN");
+
+using var client = new HttpClient();
+client.DefaultRequestHeaders.UserAgent.ParseAdd("git-activity-tracker/1.0");
+client.DefaultRequestHeaders.Accept.Add(
+    new MediaTypeWithQualityHeaderValue("application/vnd.github+json"));
+
+if (!string.IsNullOrWhiteSpace(token))
+{
+    client.DefaultRequestHeaders.Authorization =
+        new AuthenticationHeaderValue("Bearer", token);
+}
+
+// test: input -> get input repos
+string userName = null;
+while (true) {
+    Console.WriteLine("List all my repos? Y/N");
+    string choice = Console.ReadLine();
+    if (choice == "N")
+    {
+        Console.WriteLine("Username: ");
+        userName = Console.ReadLine();
+        url = "https://api.github.com/users/" + userName + "/repos";
+        break;
+    } 
+    else
+    {
+        break;
+    }
+}
+
+var resp = await client.GetAsync(url);
+Console.WriteLine($"Status: {(int)resp.StatusCode} {resp.ReasonPhrase}");
+Console.WriteLine("---------------");
+string body = await resp.Content.ReadAsStringAsync();
+using var doc = JsonDocument.Parse(body);
+
+Console.WriteLine($"{userName}s Repositories: ");
+Console.WriteLine("---------------");
+
+foreach (var repo in doc.RootElement.EnumerateArray())
+{
+    string name = repo.GetProperty("name").GetString();
+    Console.WriteLine($"{name}");
+}
--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # git_activity_tracker
 Tracks your Git activity by communicating with GitHubs API
+* Tracks your repositories
+* Tracks Activity
+* Tracks input(username)s Activity and repositories
+
+dir: ConsoleApp
+run: dotnet run
